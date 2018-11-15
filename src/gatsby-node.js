ad7389c870974a579f9bb1583c09264df25de898
Will King <contact@wking.io>
2018-11-15 08:33:55 -0600

    Somehow missed this file

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -2,8 +2,8 @@ exports.onCreateWebpackConfig = (
   { actions, stage },
   { plugins, ...elmOptions }
 ) => {
-  const { setWebpackConfig } = actions;
-  const isDev = stage === `develop`;
+  const { setWebpackConfig } = actions
+  const isDev = stage === `develop`
 
   const elmLoader = {
     loader: require.resolve('elm-webpack-loader'),
@@ -11,23 +11,23 @@ exports.onCreateWebpackConfig = (
       debug: isDev,
       forceWatch: !isDev,
       optimize: !isDev,
-      ...elmOptions,
-    },
-  };
+      ...elmOptions
+    }
+  }
 
   const elmHotLoader = {
-    loader: require.resolve('elm-hot-webpack-loader'),
-  };
+    loader: require.resolve('elm-hot-webpack-loader')
+  }
 
   const elmRule = isDev => ({
     test: /\.elm$/,
     exclude: [/[/\\\\]elm-stuff[/\\\\]/, /[/\\\\]node_modules[/\\\\]/],
-    use: [...(isDev ? [elmHotLoader] : []), elmLoader],
-  });
+    use: [...(isDev ? [elmHotLoader] : []), elmLoader]
+  })
 
   setWebpackConfig({
     module: {
-      rules: [elmRule(isDev)],
-    },
-  });
-};
+      rules: [elmRule(isDev)]
+    }
+  })
+}
